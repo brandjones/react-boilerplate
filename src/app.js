d4ad4e09679c054ca4e011f4a75e1132fd8cb6d0
Brandon Jones <Brandon_Jones@brandons-air.home>
2018-02-21 01:20:23 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,74 @@
+import React            from 'react';
+import ReactDOM         from 'react-dom';
+import { Provider }     from 'react-redux';
+import AppRouter, { history }       from './routers/AppRouter';
+import configureStore   from './store/configureStore';
+
+import { login, logout } from './actions/auth';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
+
+
+const store = configureStore();
+
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+let hasRendered = false;
+const renderApp = () => {
+  if(!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+
+
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    store.dispatch(login(user.uid));
+    renderApp();
+    if(history.location.pathname === '/') {
+      history.push('/dashboard');
+    }
+  } else {
+      store.dispacth(logout())
+      renderApp();
+      history.push('/');
+  }
+});
+
+
+
+
+// React does not handle too many components needing access
+// to props. It's not that it can't its just that the components
+// will not be reusable which is a core feature of react. In order,
+// to maintain the state so all components can access it I need to install
+// redux which is a state container. Redux does not naturally connect
+// to react which is why I have to install the react-redux library.
+
+
+// The connect function takes the redux store or the state I want to
+// access and the second function takes the actual component I want to
+// pass the state to, in this case ExpenseList.
+
+// I created a variable called mapStateToProps (which is convention) and pass it in to the
+// the connect function which returns the state object passed from the
+// redux store. Then, finally I have access to the state on the props object
+// on the component ExpenseList.
+
+// THIS PAGE ***
+
+// The Provider is a HIGHER ORDER COMPONENT that passes the state to all of the
+// components within in it. Passes AppRouter which in turn holds all the other
+// components.
---
+++ src/store/configureStore.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+
+export default () => {
+
+  // Create State Store.
+  const store = createStore(
+    combineReducers({
+      auth: authReducer
+    }),
+     composeEnhancers(applyMiddleware(thunk))
+  );
+
+  return store;
+};
